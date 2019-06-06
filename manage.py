fc846ea5c855c65603f6a3030f8fe629996c59f8
小李 <xiaoli@163.com>
2019-06-06 20:22:27 +0800

    集成CSRFProtect

--- manage.py
+++ manage.py
@@ -1,12 +1,18 @@
 """
-1、集成sqlalchemy
-2、集成redis
+1、集成配置类
+2、集成sqlalchemy     pip install flask_sqlalchemy
+3、集成redis          pip install flask_redis
+4、集成CRSFProtect
+5、集成flask_session  pip install flask_session
 """
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
+from flask_wtf import CSRFProtect
+
 
 app = Flask(__name__)
+# 1、集成配置类
 class Config(object):
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/TalkingShow"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -15,8 +21,13 @@ class Config(object):
 
 
 app.config.from_object(Config)
+# ２、集成sqlalchemy
 db = SQLAlchemy(app)
+# ３、集成redis
 redis_sroce = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# ４、集成CSRFProtect
+CSRFProtect(app)
+
 
 @app.route("/")
 def index():
