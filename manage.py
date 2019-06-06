eb771c2ff44c339cce0e9bdce0ce7009c39a3509
小李 <xiaoli@163.com>
2019-06-06 22:30:08 +0800

    集成manager和migrate

--- manage.py
+++ manage.py
@@ -2,18 +2,25 @@
 1、集成配置类
 2、集成sqlalchemy     pip install flask_sqlalchemy
 3、集成redis          pip install flask_redis
-4、集成CRSFProtect
+4、集成CRSFProtect    pip install falsk_wtf
 5、集成flask_session  pip install flask_session
+6、集成falsk_script   pip install falsk_script
+7、集成flask_migrate  pip install falsk_migrate
 """
-from flask import Flask
+from flask import Flask,session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf import CSRFProtect
 from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 # 1、集成配置类
 class Config(object):
+    DEBUG = True
+    SERETC_KEY = "erytweguifhu"
+
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/TalkingShow"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -34,9 +41,14 @@ db = SQLAlchemy(app)
 redis_sroce = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # ４、集成CSRFProtect
 CSRFProtect(app)
-# 5、集成Session
+# 5、集成flask_session
 # 说明：flask中Session是用户保存用户数据的容器（上下文），而flask_session是指定session指定保存路径
 Session(app)
+# 6、集成falsk_manager
+manager = Manager(app)
+# 7、集成flask_migrate
+Migrate(app, db)
+manager.add_command("db", MigrateCommand)
 
 
 
@@ -45,9 +57,10 @@ Session(app)
 
 @app.route("/")
 def index():
-    redis_sroce.set("name", "laoli")
+    # redis_sroce.set("name", "laoli")
+    session["name"] = "laoli"
     return "index"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
