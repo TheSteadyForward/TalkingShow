a36b0eb6ec66f14f970618f700dfb73563143b6d
小李 <xiaoli@163.com>
2019-06-08 18:44:17 +0800

    添加静态文件，并首页直接访问首页页面

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,9 @@
+from flask import render_template
+
 from . import index_blu
 from info import redis_store
 
 @index_blu.route("/")
 def index():
-    redis_store.set("age", "小邱")
-    return "hello word"
+
+    return render_template("news/index.html")
