a8143831752b9b6840eb9763fd2d448e91a450b9
小李 <xiaoli@163.com>
2019-06-08 18:55:49 +0800

    favicon.ico显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,15 @@
-from flask import render_template
-
+from flask import render_template, redirect, current_app, send_file
 from . import index_blu
-from info import redis_store
+
+
 
 @index_blu.route("/")
 def index():
-
     return render_template("news/index.html")
+
+
+@index_blu.route("/favicon.ico")
+def favicon():
+    # return redirect("/static/news/favicon.ico")
+    # return current_app.send_static_file("news/favicon.ico")
+    return send_file("static/news/favicon.ico")
