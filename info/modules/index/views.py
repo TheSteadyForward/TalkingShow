aa589242a2eaba236c9f1a510bb705ef7682a3b6
小李 <xiaoli@163.com>
2019-06-09 23:21:45 +0800

    添加用户登录显示用户信息

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,11 +1,33 @@
-from flask import render_template, redirect, current_app, send_file
+from flask import render_template, redirect, current_app, send_file, session
+
+from info.models import User
 from . import index_blu
 
 
 
 @index_blu.route("/")
 def index():
-    return render_template("news/index.html")
+    """
+    1.获取session值
+    2.通过session值查找是否存在
+    3.传递参数
+    :return:
+    """
+    user_id = session.get("user_id")
+
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    data = {
+        "user_info":user.to_dict() if user else None
+    }
+
+    return render_template("news/index.html",
+                           data=data)
 
 
 @index_blu.route("/favicon.ico")
